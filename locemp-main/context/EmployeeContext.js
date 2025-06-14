c5e37112dcf87ce381d7e761b6495106b9fb4ca2
Samyuktha113 <150355146+Samyuktha113@users.noreply.github.com>
2025-06-14 13:27:42 +0530

    Update EmployeeContext.js

--- locemp-main/context/EmployeeContext.js
+++ locemp-main/context/EmployeeContext.js
@@ -1,35 +1,3 @@
-// context/EmployeeContext.js
-// import React, { createContext, useState, useContext } from 'react';
-
-// const EmployeeContext = createContext();
-
-// export const useEmployee = () => useContext(EmployeeContext);
-
-// export const EmployeeProvider = ({ children }) => {
-//     const [employeeData, setEmployeeData] = useState(null);
-
-//     return (
-//         <EmployeeContext.Provider value={{ employeeData, setEmployeeData }}>
-//             {children}
-//         </EmployeeContext.Provider>
-//     );
-// };
-
-
-/* import React, { createContext, useState } from 'react';
-
-export const EmployeeContext = createContext();
-
-export const EmployeeProvider = ({ children }) => {
-  const [employee, setEmployee] = useState({ eid: '12345' }); // Sample or actual logic to fetch employee ID
-
-  return (
-    <EmployeeContext.Provider value={employee}>
-      {children}
-    </EmployeeContext.Provider>
-  );
-};
- */
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { db } from '../services/firebaseAuth'; // Import Firebase configuration
 import { collection, onSnapshot, query, orderBy } from 'firebase/firestore';
